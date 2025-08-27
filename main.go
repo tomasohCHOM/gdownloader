28d81a394796d501fb0d99e0d6ab6a317db41d0f
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-08-27 15:23:00 -0700

    update sample code & ignore token.json

--- main.go
+++ main.go
@@ -76,28 +76,29 @@ func main() {
 		log.Fatalf("Unable to read client secret file: %v", err)
 	}
 
+	// If modifying these scopes, delete your previously saved token.json.
 	config, err := google.ConfigFromJSON(b, drive.DriveMetadataReadonlyScope)
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
-
 	client := getClient(config)
+
 	srv, err := drive.NewService(ctx, option.WithHTTPClient(client))
 	if err != nil {
 		log.Fatalf("Unable to retrieve Drive client: %v", err)
 	}
 
-	r, err := srv.Files.List().PageSize(10).Fields("nextPageToken, files(id, name)").Do()
+	r, err := srv.Files.List().PageSize(10).
+		Fields("nextPageToken, files(id, name)").Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve files: %v", err)
 	}
-
-	fmt.Println("Files")
+	fmt.Println("Files:")
 	if len(r.Files) == 0 {
-		fmt.Println("No files")
+		fmt.Println("No files found.")
 	} else {
-		for _, file := range r.Files {
-			fmt.Printf("%s (%s)\n", file.Name, file.Id)
+		for _, i := range r.Files {
+			fmt.Printf("%s (%s)\n", i.Name, i.Id)
 		}
 	}
 }
